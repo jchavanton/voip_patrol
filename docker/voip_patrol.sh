fee509f938d86519f475efbc40620ce74f0f798c
Julien Chavanton <jchavanton@gmail.com>
2018-07-24 10:21:42 -0500

    docker example network host mode

--- docker/voip_patrol.sh
+++ docker/voip_patrol.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-
 DIR_PREFIX=`pwd`
-echo $DIR_PREFIX
 IMAGE=voip_patrol:latest
 docker stop voip_patrol
 docker rm voip_patrol
-
-docker run -d \
-	--name=voip_patrol \
-	--volume $DIR_PREFIX/../xml:/xml \
+docker run -d --net=host --name=voip_patrol --volume $DIR_PREFIX/../xml:/xml \
 	$IMAGE \
 	./git/voip_patrol/voip_patrol --port 5555 --conf /xml/tls_server.xml -l /log/voip_patrol.log --output result.json
 #	/bin/sh -c "tail -f /dev/null"

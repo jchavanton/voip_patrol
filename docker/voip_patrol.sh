fac8bdcebb5e6795a543f7ef683cf642550d22d1
Julien Chavanton <jchavanton@gmail.com>
2019-10-31 14:38:56 +0000

    update docker files to debian and pjproject

--- docker/voip_patrol.sh
+++ docker/voip_patrol.sh
@@ -5,5 +5,5 @@ docker stop voip_patrol
 docker rm voip_patrol
 docker run -d --net=host --name=voip_patrol --volume $DIR_PREFIX/../xml:/xml \
 	$IMAGE \
-	./git/voip_patrol/voip_patrol --port 5555 --conf /xml/tls_server.xml -l /log/voip_patrol.log --output result.json
+	./git/voip_patrol/voip_patrol -p 5555 -c /xml/tls_server.xml -l /log/voip_patrol.log -o result.json
 #	/bin/sh -c "tail -f /dev/null"

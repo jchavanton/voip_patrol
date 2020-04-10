9615c567c5194403a607d48e615d58b136f641cc
Julien Chavanton <jchavanton@gmail.com>
2020-04-10 10:23:17 -0700

    make sure docker image version will use lastest

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-TAG="0.2.1"
+TAG="0.2.2"
 docker build . --no-cache -t voip_patrol
 docker tag voip_patrol:latest jchavanton/voip_patrol:latest
 docker tag voip_patrol:latest jchavanton/voip_patrol:${TAG}
-# docker push voip_patrol:latest
-# docker push flowroute/voip_patrol:${TAG}
+# docker push jchavanton/voip_patrol:latest
+# docker push jchavanton/voip_patrol:${TAG}

384b86c157f4465a92cc5cd070b1c58e307f64a6
Julien Chavanton <jchavanton@gmail.com>
2022-12-16 08:39:55 -0500

    add comment to docker build to remember to push

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -3,5 +3,5 @@ TAG="0.3.6"
 docker build . --no-cache -t voip_patrol
 docker tag voip_patrol:latest jchavanton/voip_patrol:latest
 docker tag voip_patrol:latest jchavanton/voip_patrol:${TAG}
-# docker push jchavanton/voip_patrol:latest
-# docker push jchavanton/voip_patrol:${TAG}
+echo "Don't forget to push !"
+echo "docker push jchavanton/voip_patrol:latest && docker push jchavanton/voip_patrol:${TAG}"

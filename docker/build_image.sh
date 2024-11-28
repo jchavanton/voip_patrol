d8ae69b1980c71212d22c66fd4583dab437fa8bc
jchavanton <jchavanton@gmail.com>
2024-11-28 16:13:44 -0500

    Bump version: 0.7.6 → 0.7.7

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TAG="0.7.6"
+TAG="0.7.7"
 docker build . --no-cache -t voip_patrol
 docker tag voip_patrol:latest jchavanton/voip_patrol:latest
 docker tag voip_patrol:latest jchavanton/voip_patrol:${TAG}
